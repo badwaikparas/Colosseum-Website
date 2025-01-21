032d70b7e4ab29831d0b9f67d050f141eefa689a
Harshal292004 <malaniharshal95@gmail.com>
2025-01-21 20:01:29 +0530

    init

---
+++ app/(pages)/page.jsx
@@ -0,0 +1,16 @@
+import AboutUs from "../components/AboutUs";
+import Events from "../components/Events";
+import Home from "../components/Home";
+import Gallery from "../components/Gallery";
+import Sponsors from "../components/Sponsors";
+export default function Page() {
+  return (
+    <div>
+      <Home></Home>
+      <Events></Events>
+      <AboutUs></AboutUs>
+      <Gallery></Gallery>
+      <Sponsors></Sponsors>
+    </div>
+  );
+}
